eb9dc70f394016e03e10a2b7f2fd24a3aa1914b9
Abel Simões <abelrmsimoes@gmail.com>
2023-06-02 17:41:43 -0300

    Iniciando configuracoes Wallhaven

--- random_wallpaper_api.py
+++ random_wallpaper_api.py
@@ -1,5 +1,6 @@
 import os
 import ctypes
+import random
 import requests
 import urllib.parse
 from dotenv import load_dotenv
@@ -13,11 +14,23 @@ class RandomWallpaperAPI:
 
         # Obtém o valor da variável de ambiente "API_KEY" e realiza a pesquisa
         load_dotenv()
-        client_id = os.getenv("UNSPLASH_KEY")
-        url = f"https://api.unsplash.com/photos/random?client_id={client_id}&query={urllib.parse.quote(search_term)}&orientation={orientation_value}&featured={featured_image}"
+        api_key = os.getenv("UNSPLASH_KEY")
+        url = f"https://api.unsplash.com/photos/random?client_id={api_key}&query={urllib.parse.quote(search_term)}&orientation={orientation_value}&featured={featured_image}"
 
         # Verifica se existe conexão com a internet
-        self.check_internet_connection(url)
+        try:
+            response = requests.get(url, allow_redirects=True)
+            if response.status_code != 200:
+                for i in range(3):
+                    response = requests.get(url, allow_redirects=True)
+                    if response.status_code == 200:
+                        break
+                    else:
+                        continue
+            data = response.json()
+        except requests.exceptions.ConnectionError:
+            raise ValueError(
+                "Não foi possível realizar a pesquisa. Verifique sua conexão com a internet.")
 
         # Verifica se a pesquisa retornou uma imagem
         try:
@@ -41,41 +54,96 @@ class RandomWallpaperAPI:
         for photo_file in photo_files[9:]:
             os.remove(os.path.join(user_pictures_dir, photo_file))
 
-        # Verifica se a imagem já existe na pasta "Pictures" do usuário
         if not os.path.isfile(photo_path):
-            # Baixar a imagem e salvá-la na pasta "Pictures" do usuário
-            response = requests.get(photo_url)
+            response = requests.get(photo_url, allow_redirects=True)
+            # se o status for diferente de 200, tenta novamente por 3 vezes
+            if response.status_code != 200:
+                for i in range(3):
+                    response = requests.get(photo_url, allow_redirects=True)
+                    if response.status_code == 200:
+                        break
+                    else:
+                        continue
             with open(photo_path, "wb") as photo_file:
                 photo_file.write(response.content)
 
-        # Definir a imagem como papel de parede
-        SPI_SETDESKWALLPAPER = 20
-        ctypes.windll.user32.SystemParametersInfoW(
-            SPI_SETDESKWALLPAPER, 0, os.path.abspath(photo_path), 0)
+        win32_set_wallpaper(photo_path)
 
-    # [TODO] Função para buscar imagens da api do Wallhaven
-    def set_wallhaven_wallpaper(self, search_term):
+    # Função para buscar imagens da api do Wallhaven
+    def set_wallhaven_wallpaper(self, search_term, ratio_value, nsfw_value):
         self.search_term = search_term
-        # Obtém o valor da variável de ambiente "API_KEY" e realiza a pesquisa
+        self.ratio_value = ratio_value
+        self.nsfw_value = nsfw_value
+
         load_dotenv()
-        client_id = os.getenv("WALHAVEN_KEY")
+        api_key = os.getenv("WALLHAVEN_KEY")
 
-        # traz 1 imagem aleatória
-        url = f"https://wallhaven.cc/api/v1/search?apikey={client_id}&q={urllib.parse.quote(search_term)}&sorting=random&order=desc&page=1&seed=1"
+        array_term = list(filter(None, search_term.split(";")))
+        search_term = random.choice(array_term)
 
-        # url = f"https://wallhaven.cc/api/v1/search?apikey={client_id}&q={urllib.parse.quote(search_term)}"
-        print(url)
+        if nsfw_value == "true":
+            purity = "111"
+        else:
+            purity = "100"
+
+        url = f"https://wallhaven.cc/api/v1/search?apikey={api_key}&q={urllib.parse.quote(search_term)}&ratios={ratio_value}&purity={purity}&sorting=random"
 
         # Verifica se existe conexão com a internet
-        self.check_internet_connection(url)
-
-    def check_internet_connection(self, url):
         try:
-            response = requests.get(url)
+            response = requests.get(url, allow_redirects=True)
+            # se o status for diferente de 200, tenta novamente por 3 vezes
+            if response.status_code != 200:
+                for i in range(3):
+                    response = requests.get(url, allow_redirects=True)
+                    if response.status_code == 200:
+                        break
+                    else:
+                        continue
             data = response.json()
         except requests.exceptions.ConnectionError:
             raise ValueError(
                 "Não foi possível realizar a pesquisa. Verifique sua conexão com a internet.")
+
+        # Verifica se a pesquisa retornou uma imagem
+        try:
+            photo_url = data['data'][0]['path']
+            photo_filename = data['data'][0]['path'].split('/')[-1]
+        except KeyError:
+            raise ValueError(
+                "Não foi possível encontrar uma imagem com esses parâmetros.")
+
+        # Definir o caminho completo para a pasta "Pictures" do usuário
+        user_pictures_dir = os.path.join(os.path.expanduser("."), "Pictures")
+
+        photo_path = os.path.join(user_pictures_dir, photo_filename)
+
+        # Mantem apenas as 10 imagens mais recentes na pasta "Pictures" do usuário
+        photo_files = os.listdir(user_pictures_dir)
+        photo_files.sort(key=lambda x: os.path.getmtime(
+            os.path.join(user_pictures_dir, x)), reverse=True)
+        for photo_file in photo_files[9:]:
+            os.remove(os.path.join(user_pictures_dir, photo_file))
+
+        response = requests.get(photo_url, allow_redirects=True)
+        # se o status for diferente de 200, tenta novamente por 3 vezes
+        if response.status_code != 200:
+            for i in range(3):
+                response = requests.get(photo_url, allow_redirects=True)
+                if response.status_code == 200:
+                    break
+                else:
+                    continue
+
+        with open(photo_path, "wb") as photo_file:
+            photo_file.write(response.content)
+
+        win32_set_wallpaper(photo_path)
+
+
+def win32_set_wallpaper(image_path):
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoW(
+        SPI_SETDESKWALLPAPER, 0, os.path.abspath(image_path), 0)
 
 
 if __name__ == '__main__':
--- random_wallpaper_gui.py
+++ random_wallpaper_gui.py
@@ -67,7 +67,7 @@ class RandomWallpaperGUI:
         if not os.path.exists(os.path.join(os.path.expanduser("."), "Pictures")):
             os.makedirs(os.path.join(os.path.expanduser("."), "Pictures"))
 
-        ## Unsplash Frame ##
+        # region ## Unsplash Frame ##
         search_label_unsplash = tk.Label(self.unsplash_frame)
         search_label_unsplash["justify"] = "center"
         search_label_unsplash["text"] = "Pesquise o termo de busca:"
@@ -121,6 +121,65 @@ class RandomWallpaperGUI:
             self.unsplash_frame, text="Somente imagens em destaque", variable=self.featured_unsplash)
         self.featured_unsplash_check.place(
             x=5, y=165, width=width * 0.5 - 10, height=20)
+        # endregion
+
+        # region ## Wallhaven Frame ##
+        search_label_wallhaven = tk.Label(self.wallhaven_frame)
+        search_label_wallhaven["justify"] = "center"
+        search_label_wallhaven["text"] = "Pesquise o termo de busca:"
+        search_label_wallhaven.place(
+            x=5, y=5, width=width * 0.5 - 10, height=20)
+
+        self.search_entry_wallhaven = tk.Entry(self.wallhaven_frame)
+        self.search_entry_wallhaven["justify"] = "center"
+        self.search_entry_wallhaven.insert(
+            0, "space; mountains; forest; rain;")
+        self.search_entry_wallhaven.place(
+            x=5, y=25, width=width * 0.5 - 10, height=25)
+
+        time_label_wallhaven = tk.Label(self.wallhaven_frame)
+        time_label_wallhaven["justify"] = "center"
+        time_label_wallhaven["text"] = "Tempo em minutos para a atualização:"
+        time_label_wallhaven.place(
+            x=5, y=60, width=width * 0.5 - 10, height=20)
+
+        self.time_entry_wallhaven = tk.Entry(self.wallhaven_frame)
+        self.time_entry_wallhaven["justify"] = "center"
+        self.time_entry_wallhaven.insert(0, "30")
+        self.time_entry_wallhaven.place(
+            x=5, y=80, width=width * 0.5 - 10, height=25)
+
+        self.aspect_ratio_wallhaven = tk.StringVar()
+        self.aspect_ratio_wallhaven.set("16x9")
+        self.aspect_ratio_wallhaven_label = tk.Label(self.wallhaven_frame)
+        self.aspect_ratio_wallhaven_label["justify"] = "center"
+        self.aspect_ratio_wallhaven_label["text"] = "Proporção da imagem:"
+        self.aspect_ratio_wallhaven_label.place(
+            x=5, y=115, width=width * 0.5 - 10, height=20)
+
+        self.aspect_ratio_wallhaven_16x9 = tk.Radiobutton(
+            self.wallhaven_frame, text="16x9", variable=self.aspect_ratio_wallhaven, value="16x9")
+        self.aspect_ratio_wallhaven_16x9.place(
+            x=5, y=135, width=100, height=20)
+
+        self.aspect_ratio_wallhaven_9x16 = tk.Radiobutton(
+            self.wallhaven_frame, text="9x16", variable=self.aspect_ratio_wallhaven, value="9x16")
+        self.aspect_ratio_wallhaven_9x16.place(
+            x=105, y=135, width=100, height=20)
+
+        self.aspect_ratio_wallhaven_1x1 = tk.Radiobutton(
+            self.wallhaven_frame, text="1x1", variable=self.aspect_ratio_wallhaven, value="1x1")
+        self.aspect_ratio_wallhaven_1x1.place(
+            x=205, y=135, width=100, height=20)
+
+        self.nsfw_wallhaven = tk.IntVar()
+        self.nsfw_wallhaven.set(0)
+        self.nsfw_wallhaven_check = tk.Checkbutton(
+            self.wallhaven_frame, text="Ativar imagens NSFW", variable=self.nsfw_wallhaven)
+        self.nsfw_wallhaven_check.place(
+            x=5, y=165, width=width * 0.5 - 10, height=20)
+
+        # endregion
 
         ## Button Frame ##
         search_button = tk.Button(self.button_frame)
@@ -157,6 +216,12 @@ class RandomWallpaperGUI:
             try:
                 self.config.read("config.ini")
 
+                if self.config.has_section("RandomWallpaper"):
+                    for index in range(self.notebook.index("end")):
+                        tab_name = self.notebook.tab(index, "text")
+                        if tab_name == self.config["RandomWallpaper"]["visible_tab"]:
+                            self.notebook.select(index)
+
                 # Carrega as configurações da seção Unsplash
                 if self.config.has_section("Unsplash"):
                     self.search_entry_unsplash.delete(0, tk.END)
@@ -170,7 +235,18 @@ class RandomWallpaperGUI:
                     self.featured_unsplash.set(
                         self.config["Unsplash"]["featured_value"])
 
-                # [TODO] Carrega as configurações da seção Wallhaven
+                # Carrega as configurações da seção Wallhaven
+                if self.config.has_section("Wallhaven"):
+                    self.search_entry_wallhaven.delete(0, tk.END)
+                    self.search_entry_wallhaven.insert(
+                        0, self.config["Wallhaven"]["search_term"])
+                    self.time_entry_wallhaven.delete(0, tk.END)
+                    self.time_entry_wallhaven.insert(
+                        0, self.config["Wallhaven"]["time_interval"])
+                    self.aspect_ratio_wallhaven.set(
+                        self.config["Wallhaven"]["ratio_value"])
+                    self.nsfw_wallhaven.set(
+                        self.config["Wallhaven"]["nsfw_value"])
 
             except:
                 messagebox.showwarning(
@@ -198,7 +274,6 @@ class RandomWallpaperGUI:
                 self.image_label.config(image=picture_photo)
                 self.image_label.image = picture_photo
             except:
-                self.image_label.config(text="Erro ao carregar a imagem")
                 os.remove(picture_file)
 
         else:
@@ -232,6 +307,11 @@ class RandomWallpaperGUI:
                 1000, self.update_time_remaining, time_remaining - 1)
 
     def set_wallpaper(self):
+
+        self.config["RandomWallpaper"] = {
+            "visible_tab": self.notebook.tab(self.notebook.select(), "text"),
+        }
+
         # Cancela o after do wallpaper anterior (se existir)
         if "after_wallpaper" in dir(self):
             root.after_cancel(self.after_wallpaper)
@@ -264,14 +344,29 @@ class RandomWallpaperGUI:
                     self.config.write(configfile)
 
             if self.notebook.tab(self.notebook.select(), "text") == "Wallhaven":
-                # exibe mensagem de que o recurso está em desenvolvimento
-                messagebox.showinfo(
-                    "Em desenvolvimento", "Este recurso está em desenvolvimento")
+                search_term = self.search_entry_wallhaven.get()
+                time_interval = int(
+                    self.time_entry_wallhaven.get()) * 60 * 1000
+                ratio_value = self.aspect_ratio_wallhaven.get()
+                nsfw_value = self.nsfw_wallhaven.get()
 
-                self.api.set_wallhaven_wallpaper('mountains')
+                # Tenta definir o papel de parede
+                self.api.set_wallhaven_wallpaper(
+                    search_term, ratio_value, nsfw_value)
+                self.after_wallpaper = self.master.after(
+                    time_interval, self.set_wallpaper)
 
-            self.preview_wallpaper()
+                self.config["Wallhaven"] = {
+                    "search_term": search_term,
+                    "time_interval": time_interval // 1000 // 60,
+                    "ratio_value": ratio_value,
+                    "nsfw_value": nsfw_value
+                }
+                with open("config.ini", "w") as configfile:
+                    self.config.write(configfile)
+
             self.update_time_remaining(time_interval // 1000)
+            self.preview_wallpaper()
 
         except ValueError as e:
             messagebox.showerror("Erro", str(e))
